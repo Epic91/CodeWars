e62f22c304018ee3d2a65568183f60ad8dd4531c
Evelyn Hernandez <hernandeze791@gmail.com>
2021-06-21 00:53:44 -0500

    new sql challenge

---
+++ 8_kyu/people.sql
@@ -0,0 +1,6 @@
+-- For this challenge you need to create a SELECT statement that will return all columns from the people table WHERE their age is over 50
+
+SELECT * 
+FROM people
+WHERE age > 50
+ORDER BY age DESC
